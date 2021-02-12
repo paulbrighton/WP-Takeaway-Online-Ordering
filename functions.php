d15ce154fa975dfb83243ffbd8c30d6656d980bb
Paul Brighton <brighton.paul@yahoo.com>
2021-02-12 14:23:14 +0000

    Added some functions for preloading

--- functions.php
+++ functions.php
@@ -32,3 +32,54 @@ function my_exclude_ipad_and_tablets( $is_mobile ) {
   return $is_mobile;
 }
 add_filter( 'wp_is_mobile','my_exclude_ipad_and_tablets' );
+
+function add_rel_preload($html, $handle, $href, $media) {
+    
+  if (is_admin())
+      return $html;
+
+   $html = <<<EOT
+<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' crossorigin='anonymous' />
+EOT;
+  return $html;
+}
+add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
+
+add_action('wp_head', 'fouc_protect_against');
+/**
+ * Combat FOUC in WordPress
+ * @link https://stackoverflow.com/questions/3221561/eliminate-flash-of-unstyled-content
+ */
+function fouc_protect_against () {
+    ?>
+        <style type="text/css">
+            .hidden {display:none;}
+        </style>
+        <script type="text/javascript">
+         jQuery('html').addClass('hidden');
+	            
+	 jQuery(document).ready(function($) {		            
+	    $('html').removeClass('hidden');	            
+	 });  
+        </script>
+    <?php
+
+}
+
+function add_defer_attribute( $tag, $handle ) {
+
+  $scripts_to_defer = array(
+      'jquery',
+      'jquery-migrate',
+      'bootstrap-bundle-js'
+  );
+
+  foreach ( $scripts_to_defer as $defer_script ) {
+      if ( $defer_script === $handle ) {
+          return str_replace( "src", "defer src", $tag );
+      }
+  }
+
+  return $tag;
+}
+add_filter( 'script_loader_tag', 'add_defer_attribute', 10, 2 );
