8c33f2062fbf601de0c9a55adb8657fa1a1430e6
Paul Brighton <brighton.paul@yahoo.com>
2021-02-12 10:13:18 +0000

    Added hero sections to all pages

--- front-page.php
+++ front-page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <div class="c-front-page">
-  <div style="background: url('<?php the_field('home_hero_image'); ?>') center center; background-size: cover; background-repeat: no-repeat;" class="c-home-hero">
-    <div class="c-home-hero__text">
-      <h1 class="c-home-hero__text-top"><?php the_field('home_hero_text_top'); ?></h1>
-      <p class="c-home-hero__text-middle"><?php the_field('home_hero_text_middle'); ?></p>
-      <div class="c-home-hero__text-btn"><?php echo do_shortcode('[restaurant-menu-and-ordering class="glf-btn-basic" ruid="1750f287-d944-4c01-8f21-68c91ed10d5c"]'); ?></div>
+  <div style="background: url('<?php the_field('home_hero_image'); ?>') center center; background-size: cover; background-repeat: no-repeat;" class="c-hero">
+    <div class="c-hero__text">
+      <h1 class="c-hero__text-top"><?php the_field('home_hero_text_top'); ?></h1>
+      <p class="c-hero__text-middle"><?php the_field('home_hero_text_middle'); ?></p>
+      <div class="c-hero__text-btn"><?php echo do_shortcode('[restaurant-menu-and-ordering class="glf-btn-basic" ruid="1750f287-d944-4c01-8f21-68c91ed10d5c"]'); ?></div>
     </div>
   </div>
 
--- page-contact.php
+++ page-contact.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-contact page content here
+<div class="c-contact">
+  <div style="background: url('<?php the_field('contact_hero_image'); ?>') center center; background-size: cover; background-repeat: no-repeat;" class="c-hero c-hero-contact">
+    <div class="c-hero__text">
+      <h1 class="c-hero__text-top"><?php the_field('contact_hero_text_top'); ?></h1>
+      <p class="c-hero__text-middle"><?php the_field('contact_hero_text_middle'); ?></p>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
